a3a9fb75443546c545cccc8d0ec365cc3a11dccf
dvdissan <dvdissan@ualberta.ca>
2026-03-04 12:26:26 -0800

    has city passed

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -13,9 +13,9 @@ public class CustomList {
     public void addCity(City city) {
         cities.add(city);
     }
-//    public boolean hasCity(City city) {
-//        return cities.contains(city);
-//    }
+    public boolean hasCity(City city) {
+        return cities.contains(city);
+    }
 
 //
 //    public void deleteCity(City city){
